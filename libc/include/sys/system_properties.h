6c3946707cc0e165d501fcd467f4ab3844f94285
Chih-Wei Huang <cwhuang@linux.org.tw>
2009-06-30 17:43:31 +0800

    change PROP_NAME_MAX to 128

--- libc/include/sys/system_properties.h
+++ libc/include/sys/system_properties.h
@@ -31,7 +31,7 @@
 
 typedef struct prop_info prop_info;
 
-#define PROP_NAME_MAX   32
+#define PROP_NAME_MAX   128
 #define PROP_VALUE_MAX  92
 
 /* Look up a system property by name, copying its value and a
